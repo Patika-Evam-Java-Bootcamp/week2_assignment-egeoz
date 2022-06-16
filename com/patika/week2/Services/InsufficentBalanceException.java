ba196b3ab097f4b2846f6ad0cf0858450527d02a
egeoz <ege.oz@tutanota.com>
2022-06-16 16:09:29 +0300

    Fixed exception formatting

--- com/patika/week2/Services/InsufficentBalanceException.java
+++ com/patika/week2/Services/InsufficentBalanceException.java
@@ -2,7 +2,7 @@ package com.patika.week2.Services;
 
 public class InsufficentBalanceException extends Exception{
     public InsufficentBalanceException(double currentBalance, double invoiceAmount){
-        System.err.printf("Account has insufficient balance:%nCurrent balance: %d%nInvoice amount: %d%n", currentBalance, invoiceAmount);
+        System.err.printf("Account has insufficient balance. Current balance is %d and invoice amount is %d.%n", currentBalance, invoiceAmount);
     }
     
 }
